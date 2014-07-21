c1e7824dfbaa9f9666e586f8ace5781ce28467ac
TrueHazi <truehazi@TrueHazitekiMacBook-Air.local>
2014-07-22 00:36:00 +0800

    user wget instead of curl

--- HazisBingDesktop.sh
+++ HazisBingDesktop.sh
@@ -12,7 +12,7 @@ bing="www.bing.com"
 #
 # The idx parameter determines where to start from. 0 is the current day,
 # 1 the previous day, etc.
-xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=zh-CN"
+xmlURL="http://www.bing.com/HPImageArchive.aspx?format=xml&idx=0&n=1&mkt=en-US"
 
 # $saveDir is used to set the location where Bing pics of the day
 # are stored.  $HOME holds the path of the current user's home directory
@@ -23,13 +23,13 @@ logFile=$saveDir"log.txt"
 mkdir -p $saveDir
 
 # The desired Bing picture resolution to download
-# Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1200"
-# It is a pity that the 1920x1200 picture has a watermark
+# Valid options: "_1024x768" "_1280x720" "_1366x768" "_1920x1080" "_1920x1200"
 desiredPicRes="_1920x1080"
 
 # The file extension for the Bing pic
 picExt=".jpg"
 
+date >> $logFile
 for i in 1 2 3 4 5
 do
     # Wait for the network
@@ -48,12 +48,12 @@ do
         continue
     fi
     # Download the Bing pic of the day at desired resolution
-    curl -s -o $saveDir$picName $desiredPicURL
+    wget -t 0 -T 5 -c -O $saveDir$picName $desiredPicURL
     exitCode=$?
     echo $picName $exitCode >> $logFile
     if [ 0 == $exitCode ]
     then
-    # Set as wallpaper
+        # Set as wallpaper
         osascript -e "set fp to \"$saveDir$picName\"" -e 'set fp to POSIX file fp as string' -e 'tell Application "Finder"' -e 'set the desktop picture to fp as alias' -e 'end tell'
         break
     fi
